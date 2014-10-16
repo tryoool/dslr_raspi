cfb0cebcc79f0e8a084983c04b8efe8bea652a66
tryoool <tryoool@gmail.com>
2014-10-16 01:26:48 -0700

    modified two files

--- c_language/test.c
+++ c_language/test.c
@@ -3,7 +3,7 @@
 int main()
 {
 	int a,b;
-	scanf ("%d %d", &a, &b);
+	printf("\n%d\n", a+b);
 	printf("\n%d\n", a+b);
 	return 0;
 }
--- test.c
+++ test.c
@@ -5,5 +5,6 @@ int main()
 	int a,b;
 	scanf ("%d %d", &a, &b);
 	printf("\n%d\n", a+b);
+	printf("Welson.Liu");
 	return 0;
 }
